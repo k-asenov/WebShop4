236b407097b4f119682ca62a4613257cb91749f0
k-asenov <k.asenov2020@pmg-pernik.com>
2025-01-21 10:21:33 +0200

    4

---
+++ WebShopApp/WebShopApp.Core/Services/CategoryService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WebShopApp.Core.Contracts;
+using WebShopApp.Infrastructure.Data;
+using WebShopApp.Infrastructure.Data.Domain;
+
+namespace WebShopApp.Core.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Category CategoryById(int categoryId)
+        {
+            return _context.Categories.Find(categoryId);
+        }
+        public List<Category> GetCategories()
+        {
+            List<Category> categories = _context.Categories.ToList();
+            return categories;
+        }
+
+        public Category GetCategoriesById(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetProductsByCategory(int categoryId)
+        {
+            return _context.Products.Where(x => x.CategoryId == categoryId).ToList();
+        }
+    }
+}
