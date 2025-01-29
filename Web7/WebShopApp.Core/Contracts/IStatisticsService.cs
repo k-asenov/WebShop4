088abdb66069742036a3572a995088fb55a5d470
k-asenov <k.asenov2020@pmg-pernik.com>
2025-01-29 12:28:03 +0200

    7

---
+++ Web7/WebShopApp.Core/Contracts/IStatisticsService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShopApp.Core.Contracts
+{
+    public interface IStatisticsService
+    {
+        int CountProduct();
+        int CountClient();
+        int CountOrders();
+        decimal SumOrders();
+    }
+}
---
+++ Web7/WebShopApp/Controllers/StatisticsController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using WebShopApp.Core.Contracts;
+using WebShopApp.Models.Statistic;
+
+namespace WebShopApp.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        public readonly IStatisticsService statisticsService;
+        public StatisticsController(IStatisticsService statisticsService)
+        {
+            this.statisticsService = statisticsService;
+        }
+
+        // GET: StatisticsController
+        public ActionResult Index()
+        {
+           StatisticVM statistics = new StatisticVM();
+            statistics.CountClients = statisticsService.CountClient();
+            statistics.CountProducts = statisticsService.CountProduct();    
+            statistics.CountOrders = statisticsService.CountOrders();
+            statistics.SumOrders = statisticsService.SumOrders();   
+
+            return View(statistics);
+        }
+
+        // GET: StatisticsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: StatisticsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StatisticsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StatisticsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: StatisticsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StatisticsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: StatisticsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
